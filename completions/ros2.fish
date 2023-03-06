64656889e3f7a65b11697d7caf12ebea46a3c827
Kristoffer Plagborg Bak Sørensen <kristoffer.pbs@tuta.io>
2023-03-06 21:11:42 +0100

    fix(completions): copilot generated ros3 instead of ros2...

--- completions/ros2.fish
+++ completions/ros2.fish
@@ -28,24 +28,26 @@
 #   Call `ros2 <command> -h` for more detailed usage.
 
 
+complete -c ros2 -f # disable file completion
+
 complete -c ros2 -s h -l help -d "show this help message and exit"
 
 set -l ros2_commands action bag component daemon doctor interface launch lifecycle multicast node param pkg run security service topic wtf
 
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a action -d "Various action related sub-commands"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a bag -d "Various rosbag related sub-commands"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a component -d "Various component related sub-commands"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a daemon -d "Various daemon related sub-commands"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a doctor -d "Check ROS setup and other potential issues"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a interface -d "Show information about ROS interfaces"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a launch -d "Run a launch file"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a lifecycle -d "Various lifecycle related sub-commands"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a multicast -d "Various multicast related sub-commands"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a node -d "Various node related sub-commands"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a param -d "Various param related sub-commands"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a pkg -d "Various package related sub-commands"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a run -d "Run a package specific executable"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a security -d "Various security related sub-commands"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a service -d "Various service related sub-commands"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a topic -d "Various topic related sub-commands"
-complete -c ros2 -n "not __fish_seen_subcommand_from $ros3_commands" -a wtf -d "Use `wtf` as alias to `doctor`"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a action -d "Various action related sub-commands"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a bag -d "Various rosbag related sub-commands"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a component -d "Various component related sub-commands"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a daemon -d "Various daemon related sub-commands"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a doctor -d "Check ROS setup and other potential issues"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a interface -d "Show information about ROS interfaces"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a launch -d "Run a launch file"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a lifecycle -d "Various lifecycle related sub-commands"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a multicast -d "Various multicast related sub-commands"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a node -d "Various node related sub-commands"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a param -d "Various param related sub-commands"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a pkg -d "Various package related sub-commands"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a run -d "Run a package specific executable"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a security -d "Various security related sub-commands"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a service -d "Various service related sub-commands"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a topic -d "Various topic related sub-commands"
+complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a wtf -d "Use `wtf` as alias to `doctor`"
