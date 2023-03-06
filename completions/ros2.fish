d000b7a00eaeb0a33feb5219f68635ed6e1fc6d2
Kristoffer Plagborg Bak Sørensen <kristoffer.pbs@tuta.io>
2023-03-06 21:17:59 +0100

    feat(completions): add subcommands of `ros2 component`

--- completions/ros2.fish
+++ completions/ros2.fish
@@ -51,3 +51,36 @@ complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a security
 complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a service -d "Various service related sub-commands"
 complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a topic -d "Various topic related sub-commands"
 complete -c ros2 -n "not __fish_seen_subcommand_from $ros2_commands" -a wtf -d "Use `wtf` as alias to `doctor`"
+
+
+# ros2 component ----------------------------------------------------------------------------------
+# ros2 component --help
+# usage: ros2 component [-h] Call `ros2 component <command> -h` for more detailed usage. ...
+#
+# Various component related sub-commands
+#
+# optional arguments:
+#   -h, --help            show this help message and exit
+#
+# Commands:
+#   list        Output a list of running containers and components
+#   load        Load a component into a container node
+#   standalone  Run a component into its own standalone container node
+#   types       Output a list of components registered in the ament index
+#   unload      Unload a component from a container node
+#
+#   Call `ros2 component <command> -h` for more detailed usage.
+
+set -l ros2_component_commands list load standalone types unload
+set -l ros2_component_command_descriptions \
+    "Output a list of running containers and components" \
+    "Load a component into a container node" \
+    "Run a component into its own standalone container node" \
+    "Output a list of components registered in the ament index" \
+    "Unload a component from a container node"
+
+for i in (seq (count $ros2_component_commands))
+    set -l command $ros2_component_commands[$i]
+    set -l description $ros2_component_command_descriptions[$i]
+    complete -c ros2 -n "__fish_seen_subcommand_from component; and not __fish_seen_subcommand_from $ros2_component_commands" -a $command -d $description
+end
