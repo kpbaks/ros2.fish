827463b1612be6ecd224d8328024cb7f1f4ce152
Kristoffer Plagborg Bak Sørensen <kristoffer.pbs@tuta.io>
2023-05-01 13:18:59 +0200

    fix: typo in function name

--- completions/ros2.fish
+++ completions/ros2.fish
@@ -557,7 +557,7 @@ $C -n "__fish_seen_subcommand_with_subsubcommand pkg prefix" -a "(__fish_ros2_pr
 #   --prefix PREFIX  Prefix command, which should go before the executable. Command must be wrapped in quotes if it contains spaces (e.g. --prefix 'gdb -ex run --args').
 
 $C -n "__fish_seen_subcommand_from run" -a "(__fish_ros2_print_packages)"
-$C -n "__fish_seen_subcommand_with_argument run" -a "(__fish_ros2_print_executables (commandline -opc)[3])"
+$C -n "__fish_seen_subcommand_with_argument run" -a "(__fish_ros2_print_executables_in_package (commandline -opc)[3])"
 
 
 
